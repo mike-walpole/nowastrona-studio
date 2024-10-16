2af7da3207ad98c0f6a14a48b9c4e8ee50392027
mike-walpole <walpole.mike@gmail.com>
2024-10-16 15:03:47 +0200

    nowastrona-studio

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,1 +1,29 @@
-export const schemaTypes = []
+import { defineType, defineField } from 'sanity'
+
+const faqSchema = defineType({
+  name: 'faq',
+  title: 'FAQ',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'question',
+      title: 'Question',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'answer',
+      title: 'Answer',
+      type: 'text',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'order',
+      title: 'Order',
+      type: 'number',
+      description: 'Use this field to specify the order of the FAQ items',
+    }),
+  ],
+})
+
+export const schemaTypes = [faqSchema]
